f8bf43f9c6a08ba3f84e19293423694defb55a8a
zer0Black <lxtalx@gmail.com>
2020-11-09 20:02:10 +0800

    【修改】修改自动注入字段属性的bug

--- jero-boot-base-common/src/main/java/com/jero/web/core/config/mybatis/DateMetaObjectHandler.java
+++ jero-boot-base-common/src/main/java/com/jero/web/core/config/mybatis/DateMetaObjectHandler.java
@@ -1,6 +1,7 @@
 package com.jero.web.core.config.mybatis;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
@@ -13,21 +14,30 @@ import java.util.Date;
  * @Author zero
  * @Date 2020/5/15
  **/
+@Slf4j
 @Component
 public class DateMetaObjectHandler implements MetaObjectHandler {
 
     @Override
     public void insertFill(MetaObject metaObject) {
-        LocalDateTime now = LocalDateTime.now();
-        setFieldValByName("createTime", now, metaObject);
-
+        try {
+            LocalDateTime now = LocalDateTime.now();
+            setFieldValByName("createTime", now, metaObject);
+        } catch (Exception e){
+            log.error("自动插入创建时间失败", e);
+        }
         // TODO 添加创建人为当前登陆用户
+
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        LocalDateTime now = LocalDateTime.now();
-        setFieldValByName("updateTime", now, metaObject);
+        try {
+            LocalDateTime now = LocalDateTime.now();
+            setFieldValByName("updateTime", now, metaObject);
+        } catch (Exception e){
+            log.error("自动插入更新时间失败", e);
+        }
 
         // TODO 添加更新人为当前登陆用户
     }
